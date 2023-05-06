bc7dfc3ba7d0eb45b4cf86daa63ed400c53f44c3
ysbllcby <ysabellacubay@gmail.com>
2023-05-06 10:23:20 -0600

    Delete function working.

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Create and connect mongoose database
@@ -16,7 +16,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
 
 // Schema for items
 const itemsSchema = {
-  name: String
+  name: String,
 };
 
 // Mongoose model
@@ -24,15 +24,15 @@ const Item = mongoose.model("item", itemsSchema);
 
 // New documents for items
 const item1 = new Item({
-  name: "Study"
+  name: "Study",
 });
 
 const item2 = new Item({
-  name: "Laundry"
+  name: "Laundry",
 });
 
 const item3 = new Item({
-  name: "Meditate"
+  name: "Meditate",
 });
 
 const defaultItems = [item1, item2, item3];
@@ -41,41 +41,49 @@ const defaultItems = [item1, item2, item3];
 // Item.insertMany(defaultItems);
 
 app.get("/", async (req, res) => {
-  
   Item.find()
-  .then(function (items) {
-    if (items.length === 0) {
-      Item.insertMany(items);
-    } else {
-      res.render("list", {listTitle: "Today", newListItems: items});
-    }
-  })
-  .catch(function (err) {
-    console.log(err);
+    .then(function (items) {
+      if (items.length === 0) {
+        Item.insertMany(items);
+      } else {
+        res.render("list", { listTitle: "Today", newListItems: items });
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+app.post("/", function (req, res) {
+  const itemName = req.body.newItem;
+
+  const item = new Item({
+    name: itemName,
   });
+
+  item.save();
+  res.redirect("/");
 });
 
-app.post("/", function(req, res){
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+app.post("/delete", (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  Item.deleteMany({ _id: checkedItemId })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
